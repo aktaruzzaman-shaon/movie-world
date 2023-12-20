0e972ac07f55f4c9d1d65887b36c0ed6d7100a98
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2023-12-20 18:57:46 +0600

    movie upload attempt

--- src/Components/Home/MyAddedMovies/MyAddedMovies.jsx
+++ src/Components/Home/MyAddedMovies/MyAddedMovies.jsx
@@ -1,20 +1,72 @@
 import React, { useState } from 'react';
 
 const MyAddedMovies = () => {
-    const inputRef = React.useRef();
-    const [source, setSource] = useState();
-    const handleFileChange = (event) => {
-        const file = event.target.files[0];
-        const url = URL.createObjectURL(file);
-        setSource(url);
-        console.log(file)
-    }
-    const handleChoose = (event) => {
-        inputRef.current.click();
-    };
+
+
+const [form,setFrom] = useState({
+    title:"",
+    discription: "",
+    file:null
+})
+
+const handleChange = (event)=>{
+    const inputValue = event.target.name === "file" ? event.target.files[0] : event.target.value;
+    setFrom({
+        ...form,
+        [event.target.name] : inputValue
+    })
+}
+
+ const handleSubmit = (event)=>{
+    event.preventDefault();
+    console.log({form})
+ }
+
+
+
+
+
+
+    // const inputRef = React.useRef();
+    // const [source, setSource] = useState();
+    // const handleFileChange = (event) => {
+    //     const file = event.target.files[0];
+    //     const url = URL.createObjectURL(file);
+    //     setSource(url);
+    //     console.log(file)
+    // }
+    // const handleChoose = (event) => {
+    //     inputRef.current.click();
+    // };
     return (
         <div>
-            <input
+
+            <form action="" onSubmit={handleSubmit}>
+                <div className='my-5'>
+                    <input onChange={handleChange} type="text" name="title" autoComplete='off' placeholder='title' />
+                </div>
+                <div>
+                    <textarea onChange={handleChange} type="text" name="description" autoComplete='off' placeholder='description' />
+                </div>
+                <div>
+                    <input onChange={handleChange} type="file" name="file" accept='video/mp4' placeholder='Add video file'/>
+                </div>
+                <button type='submit'>Upload video</button>
+            </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+            {/* <input
                 ref={inputRef}
                 className="VideoInput_input"
                 type="file"
@@ -30,7 +82,7 @@ const MyAddedMovies = () => {
                     controls
                     src={source}
                 />
-            )}
+            )} */}
         </div>
     );
 };
